6f0dc4c7a28bb5c19e6aa57a1af0eb219d35d073
GRuiQi <2654344573@qq.com>
2020-12-15 00:24:50 +0800

    初始化扑克牌并打乱顺序

---
+++ demo/04_playingCards/src/Test01.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 初始化一幅扑克牌
+ * 将初始化的扑克牌放到集合List
+ */
+public class Test01 {
+
+
+    public static void main(String[] args) {
+
+        String[] num = {"A","2","3","4","5","6","7","8",
+                "9","10","J","Q","K"};
+
+        String[] color = {"红桃","黑桃","方块","梅花"};
+
+        List<String> poker = new ArrayList();
+
+        //拼接扑克的花色和数字，将结果放到集合中
+        for(String s1:color){
+            for(String s2:num){
+                poker.add(s1+s2);
+            }
+        }
+        poker.add("小王");
+        poker.add("大王");
+
+        System.out.println("初始化："+poker);
+
+        //洗牌
+        Collections.shuffle(poker);
+
+        System.out.println("洗牌："+poker);
+
+        //发三张排
+        ArrayList<String> pony = new ArrayList<>();
+        ArrayList<String> jack = new ArrayList<>();
+        ArrayList<String> alice = new ArrayList<>();
+
+        //留下三张底牌
+        ArrayList<String> dipai = new ArrayList<>();
+        for (int i = 0; i < poker.size() ; i++) {
+            if(i>=poker.size()-3){
+                dipai.add(poker.get(i));
+            }else if(i%3==0){
+                pony.add(poker.get(i));
+            }else if(i%3==1){
+                jack.add(poker.get(i));
+            }else{
+                alice.add(poker.get(i));
+            }
+        }
+
+        //看牌
+        System.out.println("pony:"+pony);
+        System.out.println("jack:"+jack);
+        System.out.println("alice:"+alice);
+        System.out.println("dipai:"+dipai);
+    }
+
+}
